b5146cd857e636d5fd3c5cdaf9994781cc313421
Luigi De Rosa <luigi@epic.net>
2019-05-24 14:35:36 +0200

    Add orbit control

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # 🥘 boilerthree
 
+⚠️ Still WIP
 Boilerplate for three.js, using some juicy stuff:
+
 - [bidello](https://github.com/luruke/bidello)
 - [magicshader](https://github.com/luruke/magicshader)
 - [postfx](https://medium.com/@luruke/simple-postprocessing-in-three-js-91936ecadfb7)
 - [detect-gpu](https://github.com/TimvanScherpenzeel/detect-gpu)
 - [parcel-bundler](https://parceljs.org/)
-- [resource-loader]()
-
-
-⚠️ Still WIP
+- [resource-loader](https://github.com/englercj/resource-loader)
+- [glsl-fxaa](https://github.com/mattdesl/glsl-fxaa)
+- [orbit-control-es6](https://github.com/silviopaganini/orbit-controls-es6)
--- src/js/camera.js
+++ src/js/camera.js
@@ -1,5 +1,7 @@
 import { PerspectiveCamera, Vector3 } from 'three';
 import { component } from 'bidello';
+import OrbitControls from 'orbit-controls-es6';
+import renderer from './renderer';
 
 class Camera extends component(PerspectiveCamera) {
   constructor() {
@@ -7,6 +9,15 @@ class Camera extends component(PerspectiveCamera) {
 
     this.position.set(0, 0, -10);
     this.lookAt(new Vector3(0, 0, 0));
+    this.initOrbitControl();
+  }
+
+  initOrbitControl() {
+    const controls = new OrbitControls(this, renderer.domElement);
+
+    controls.enabled = true;
+    controls.maxDistance = 1500;
+    controls.minDistance = 0;
   }
 
   onResize({ ratio }) {
